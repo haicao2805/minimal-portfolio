075e363ad528f3fe13f80c5cf33876cee419a33b
Cao Chi Hai <haicao2805@gmail.com>
2025-08-25 16:36:11 +0700

    fix: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,11 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "@typescript-eslint/no-empty-object-type": "warn",
+    },
+  },
+  {
     ignores: [
       "node_modules/**",
       ".next/**",
